5685c31fd51ce0d8a18269b89f0f732a5a354bd7
nandoxscr <nnwwpp77@gmail.com>
2018-08-27 09:02:30 +0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,13 +1,13 @@
 
 
   
-  gg.alert("Tes") 
+gg.alert("Tes") 
 gg.setVisible(true) 
 while true do 	
         if gg.isVisible() 
             then 	
 	       gg.setVisible(false) 
-    
+    os.exit()
 end 	
 gg.sleep(100)
 end
