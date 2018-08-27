324626a95e333168124009925da03a7d99b2af65
nandoxscr <nnwwpp77@gmail.com>
2018-08-27 12:51:49 +0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,14 +1,5 @@
 
 
-  
-gg.alert('=================\n=     ChangeLog      =\n=================\n\n v.1.1\n -Release\n -New Function All\n -Bug Fixed', 'Ok') 
+  =================
+=ChangeLog      =\n=================\n\n v.1.1\n -Release\n -New Function All\n -Bug Fixed', 'Ok') 
 
-gg.setVisible(true) 
-while true do 	
-        if gg.isVisible(true) 
-            then 	
-	       gg.setVisible(false) 		
-os.exit()
-end 	
-gg.sleep(100)
-end
